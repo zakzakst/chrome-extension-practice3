ee87f5e73ea3fdf7d86a381917b8e5854c6bad87
zakzakst <e1433104@gmail.com>
2026-05-16 19:52:34 +0900

    fix

--- src/manifest.ts
+++ src/manifest.ts
@@ -4,13 +4,24 @@ import { defineManifest } from "@crxjs/vite-plugin";
 
 export default defineManifest({
   manifest_version: 3,
-  name: "My First Extension",
+  name: "タググループを開く",
   version: "1.0.0",
 
   permissions: ["bookmarks", "tabs", "tabGroups"],
 
   action: {
     default_popup: "index.html",
+    default_icon: {
+      "16": "icons/icon-16.png",
+      "32": "icons/icon-32.png",
+    },
+  },
+
+  icons: {
+    "16": "icons/icon-16.png",
+    "32": "icons/icon-32.png",
+    "48": "icons/icon-48.png",
+    "128": "icons/icon-128.png",
   },
 
   background: {
